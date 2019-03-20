9fa4044a2b9efbf338f49def41283562c218be6a
Luis <pydockdocking@gmail.com>
2019-03-20 14:55:24 +0100

    First commit, whit the parallel script

--- run_dockser_parallel_aux.sh
+++ run_dockser_parallel_aux.sh
@@ -1,53 +1,12 @@
 #!/bin/bash
-E_BADARGS=65
-echo "Searching FTDOCK MPI version"
-type ftdock >/dev/null 2>&1 || { echo >&2 "FTDock not found check the PATH variable or install the program. Aborting."; exit ${E_BADARGS}; }
-is_MPI=`ftdock | grep "MPI" | wc -l`
-if [ $is_MPI -eq 0 ]
-    then
-        echo "ftdock MPI version not found. Please, check PATH.";
-        exit ${E_BADARGS};
-fi
-if [ "$#" != "3" ] && [ "$#" != "2" ]; then
-    echo "Wrong arguments";
-    printf "Usage: `basename $0` project_name num_cpus noelec\nThe default setting of ftdock is perform the sampling with electrostatics to desactivate include the \"nonelc\" parameter.\n";
-    exit ${E_BADARGS};
-else
-    int='^[0-9]+$'
-    if ! [[ $2 =~ $int ]];then
-            echo "Wrong arguments"
-            echo "The num_processors is incorrect"
-            exit ${E_BADARGS};
-    fi
-    if ! [ -f ${1}.ini ];then
-            echo "Wrong arguments"
-            echo "The project_name is incorrect, INI file not found"
-            exit ${E_BADARGS};
-    fi
-    if [ "$3" != "noelec" ] && [ "$3" != "" ];then
-            echo "Wrong arguments"
-            echo "The electrostactic parameter is set incorectly"
-            exit ${E_BADARGS};
-    fi
-fi
-
-echo "pyDock TARGET name is: $1"
-echo "Executing ftdock MPI version using $2 cpus"
-if [ "$3" == "noelec" ];then
-	echo "Executing ftdock MPI without electrostatic"
-	mpirun -np $2 ftdock -static ${1}_rec.pdb -mobile ${1}_lig.pdb -calculate_grid 0.7 -angle_step 12 -internal -15 -surface 1.3 -keep 3 -noelec -out ${1}.ftdock
-else
-	echo "Executing ftdock MPI with electrostatic"
-	mpirun -np $2 ftdock -static ${1}_rec.pdb -mobile ${1}_lig.pdb -calculate_grid 0.7 -angle_step 12 -internal -15 -surface 1.3 -keep 3 -out ${1}.ftdock
-fi;
-echo "Calculations finished, checking for results..."
-calc_lines=`cat ${1}.ftdock | wc -l`
-if [ "$calc_lines" == "10023" ]; then
-    echo "${1}.ftdock is OK"
-else
-    echo "Error in ftdock output, please check log and errors above."
-    exit 0;
-fi
-echo "Removing temporal data..."
-rm -rf scratch_scores*.dat
-echo "Done."
+case=$1
+procID=$2
+echo "INSIDE_AUX_PROC=$procID"
+string_procID=`printf "%02d" $procID`
+mkdir tmp_pydock_$string_procID
+cp *pdb* tmp_pydock_$string_procID
+cp *ini tmp_pydock_$string_procID
+mv $case.rot.$string_procID tmp_pydock_$string_procID/$case.rot
+cd tmp_pydock_$string_procID
+$PYDOCK3_BIN $case dockser
+echo "$procID FINI"
